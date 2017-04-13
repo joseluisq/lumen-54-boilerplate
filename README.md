71a9251f756e4b16fbc6c36aac37c566464786aa
José Luis Quintana <j-l.quintana@hal-media.com>
2017-04-13 12:30:13 +0200

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Lumen 5.4 Boilerplate
+
+> [Lumen 5.4](http://lumen.laravel.com/docs) boilerplate for starting your first API.
+
+## Usage
+
+```sh
+# Clone this repository
+git clone https://github.com/joseluisq/lumen-54-boilerplate.git
+
+# Enter to cloned directory
+cd lumen-54-boilerplate
+
+# Install the dependencies
+composer install
+
+# Run the development server
+php -S localhost:8002 -t ./public
+```
+
+## Features
+
+- HTTP Exception Handler with JSON responses. [reference](https://gist.github.com/joseluisq/bea6220cca5e1441d550b27409283497)
+- Fecades and [Eloquent](https://laravel.com/docs/5.4/eloquent) enabled by default.
+- [Lumen Generators](https://github.com/webNeat/lumen-generators) with REST actions trait.
+- `.editorconfig` settings.
+- ...
+
+## Contribution
+Feel free to send issues or pull requests.
+
+## License
+MIT license
+
+© 2017 [José Luis Quintana](https://git.io/joseluisq)
---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Http\Exception\HttpResponseException;
+use Illuminate\Http\Response;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $e
+     * @return void
+     */
+    public function report(Exception $e)
+    {
+        parent::report($e);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $e
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $e)
+    {
+        if (env('APP_DEBUG')) {
+          return parent::render($request, $e);
+        }
+
+        $status = 200;
+        $success = TRUE;
+        $response = NULL;
+
+        if ($e instanceof HttpResponseException) {
+          $success = FALSE;
+          $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+          $response = $e->getResponse();
+        } elseif ($e instanceof MethodNotAllowedHttpException) {
+          $success = FALSE;
+          $status = Response::HTTP_METHOD_NOT_ALLOWED;
+          $e = new MethodNotAllowedHttpException([], 'HTTP_METHOD_NOT_ALLOWED', $e);
+        } elseif ($e instanceof NotFoundHttpException) {
+          $success = FALSE;
+          $status = Response::HTTP_NOT_FOUND;
+          $e = new NotFoundHttpException('HTTP_NOT_FOUND', $e);
+        } elseif ($e instanceof AuthorizationException) {
+          $success = FALSE;
+          $status = Response::HTTP_FORBIDDEN;
+          $e = new AuthorizationException('HTTP_FORBIDDEN', $status);
+        } elseif ($e instanceof \Dotenv\Exception\ValidationException && $e->getResponse()) {
+          $success = FALSE;
+          $status = Response::HTTP_BAD_REQUEST;
+          $e = new \Dotenv\Exception\ValidationException('HTTP_BAD_REQUEST', $status, $e);
+          $response = $e->getResponse();
+        } elseif ($e) {
+          $success = FALSE;
+          $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+          $e = new HttpException($status, 'HTTP_INTERNAL_SERVER_ERROR');
+        }
+
+        return response()->json([
+          'success' => $success,
+          'status' => $status,
+          'message' => $e->getMessage()
+        ], $status);
+    }
+}
