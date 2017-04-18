14622b8aaa12d4b9899576ac609cd0067fc6a4ee
José Luis Quintana <joseluisquintana20@gmail.com>
2017-04-18 10:41:03 +0200

    exception fixes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -53,34 +53,27 @@ class Handler extends ExceptionHandler
           return parent::render($request, $e);
         }
 
-        $status = 200;
-        $success = true;
+        $success = false;
         $response = null;
+        $status = Response::HTTP_INTERNAL_SERVER_ERROR;
 
         if ($e instanceof HttpResponseException) {
-          $success = false;
           $status = Response::HTTP_INTERNAL_SERVER_ERROR;
           $response = $e->getResponse();
         } elseif ($e instanceof MethodNotAllowedHttpException) {
-          $success = false;
           $status = Response::HTTP_METHOD_NOT_ALLOWED;
           $e = new MethodNotAllowedHttpException([], 'HTTP_METHOD_NOT_ALLOWED', $e);
         } elseif ($e instanceof NotFoundHttpException) {
-          $success = false;
           $status = Response::HTTP_NOT_FOUND;
           $e = new NotFoundHttpException('HTTP_NOT_FOUND', $e);
         } elseif ($e instanceof AuthorizationException) {
-          $success = false;
           $status = Response::HTTP_FORBIDDEN;
           $e = new AuthorizationException('HTTP_FORBIDDEN', $status);
         } elseif ($e instanceof \Dotenv\Exception\ValidationException && $e->getResponse()) {
-          $success = false;
           $status = Response::HTTP_BAD_REQUEST;
           $e = new \Dotenv\Exception\ValidationException('HTTP_BAD_REQUEST', $status, $e);
           $response = $e->getResponse();
         } elseif ($e) {
-          $success = false;
-          $status = Response::HTTP_INTERNAL_SERVER_ERROR;
           $e = new HttpException($status, 'HTTP_INTERNAL_SERVER_ERROR');
         }
 
